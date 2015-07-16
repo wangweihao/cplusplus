0d64acd1c1320454faa26d972425937dad53783d
wangweihao <578867817@qq.com>
2015-07-17 01:33:34 +0800

    try

---
+++ c++try/try.cpp
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  try.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年07月17日 01时33分15秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:   (wangweihao), 578867817@qq.com
+ *        Company:  xiyoulinuxgroup
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	
+
+	return EXIT_SUCCESS;
+}
+
+
